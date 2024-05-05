65f4888031f0887299988c177254efc3a9d5e46b
Aerendyll <143167807+Aerendyll@users.noreply.github.com>
2024-05-05 17:26:19 +0200

    feat: mongodb database migration

--- index.js
+++ index.js
@@ -1,116 +1,82 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const http = require("http");
+const mongoose = require("mongoose");
+const Person = require("./models/person");
+
 const app = express();
+const PORT = process.env.PORT;
+
 app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/", (request, response) => {
-  response.send("<h1>FullStackOpen part-3 </h1>");
+// Middleware para manejar las solicitudes de personas
+app.get("/api/persons", (request, response, next) => {
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error)); // Pasar errores a middleware de manejo de errores
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
-});
+app.get("/api/persons/:id", (request, response, next) =>{
+  Person.findByIdAndDelete(request.params.id)
+  .then(result=>{
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
 
-app.get("/api/info", (request, response) => {
-  const currentTime = new Date();
-  const totalEntries = persons.length;
-  const infoString = `<p>Phonebook has info for ${totalEntries} people <br/> ${currentTime} </p>`;
-  response.send(infoString);
-});
+app.post("/api/persons", (request,response)=>{
+  const body = request.body
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-});
-
-const generateId = () => {
-  return Math.floor(Math.random() * 1000000) + 1;
-};
-
-app.post("/api/persons", (request, response) => {
-  const body = request.body;
-
-  if (!body.name || !body.number) {
-    return response
-      .status(400)
-      .json({ error: "Name already exists or Name/Number missing" });
+  if(body.name === undefined || body.number === undefined){
+    return response.status(400).json({error: 'name or number missing'})
   }
 
-  const existingPerson = persons.find((person) => person.name === body.name);
-  if (existingPerson) {
-    return response.status(400).json({ error: "Name must be unique" });
-  }
-  const person = {
+  const person = new Person ({
     name: body.name,
-    number: body.number,
-    id: generateId(),
-  };
+    number: body.number
+  })
 
-  persons = persons.concat(person);
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
 
-  response.json(person);
-});
-
-morgan.token("postData", (request, response) => {
-  if (request.method === "POST") {
-    return JSON.stringify(request.body);
-  }
-});
-
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :postData"
-  )
-);
-
+// Middleware para manejar puntos finales desconocidos
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: " Unknown endpoint" });
+  response.status(404).send({ error: "Unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
-const server = http.createServer(app);
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+// Middleware para manejar errores
+app.use((error, request, response, next) => {
+  console.error(error);
+  response.status(500).send({ error: "Internal server error" });
 });
-//trying to solve error flyio/render
+
+const url = process.env.MONGODB_URI;
+console.log(url);
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
