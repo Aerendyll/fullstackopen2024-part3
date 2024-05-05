f368b970bda8395faae760a2a806607bd755bdc8
Aerendyll <143167807+Aerendyll@users.noreply.github.com>
2024-05-06 01:08:20 +0200

    feat: part 3d fso

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ app.use(express.static("dist"));
 app.use(cors());
 app.use(express.json());
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v.length >= 3;
+      },
+      message: props => `El nombre '${props.value}' debe tener al menos 3 caracteres.`
+    }
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\b\d{2,3}-\d{7,}\b/.test(v);
+      },
+      message: props => `${props.value} no es un número de teléfono válido. El formato debe ser XX-XXXXXXXX.`
+    }
+  },
+});
+
+
 // Middleware para manejar las solicitudes de personas
 app.get("/api/persons", (request, response, next) => {
   Person.find({})
@@ -22,46 +46,42 @@ app.get("/api/persons", (request, response, next) => {
     .catch((error) => next(error)); // Pasar errores a middleware de manejo de errores
 });
 
-app.get("/api/persons/:id", (request, response, next) =>{
-  Person.findByIdAndDelete(request.params.id)
-  .then(result=>{
-    response.status(204).end()
-  })
-  .catch(error => next(error))
-})
-
-app.delete("/api/persons/:id", (request, response, next) => {
-  const id = request.params.id;
-  Person.findByIdAndDelete(id)
-    .then((result) => {
-      if (result) {
-        response.status(204).end(); // No content, indicando que la operación de borrado fue exitosa
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
       } else {
-        response.status(404).json({ error: "Person not found" }); // Si no se encontró la persona con el ID especificado
+        response.status(404).end();
       }
     })
-    .catch((error) => next(error)); // Pasar errores a middleware de manejo de errores
+
+    .catch((error) => next(error));
 });
 
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+      console.log("Person deleted");
+    })
+    .catch((error) => next(error));
+});
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body;
 
-app.post("/api/persons", (request,response)=>{
-  const body = request.body
-
-  if(body.name === undefined || body.number === undefined){
-    return response.status(400).json({error: 'name or number missing'})
-  }
-
-  const person = new Person ({
+  const person = new Person({
     name: body.name,
-    number: body.number
-  })
+    number: body.number,
+  });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
-})
-
-
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 // Middleware para manejar puntos finales desconocidos
 const unknownEndpoint = (request, response) => {
@@ -76,6 +96,20 @@ app.use((error, request, response, next) => {
   response.status(500).send({ error: "Internal server error" });
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
 const url = process.env.MONGODB_URI;
 console.log(url);
 mongoose
@@ -87,10 +121,19 @@ mongoose
     console.error("Error connecting to MongoDB:", error.message);
   });
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
